f40f3307852bdae27310fb8a9a165ae700a3ba00
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-10-08 19:16:59 -0300

    Adicionando mongoose no projeto

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://tiagoalmeida:gostack@cluster0-opgae.mongodb.net/test?retryWrites=true&w=majority', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+});
 
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  return res.json('Hello World');
-});
+app.use(routes);
 
 app.listen(3333);
