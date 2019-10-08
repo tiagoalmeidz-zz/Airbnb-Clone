2cb8e295bd9576587e0164aa658aa22b67d5278b
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-10-08 19:47:10 -0300

    Criando SessionController

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+
+// index (retornar lista), 
+// show (lista uma unica coisa) 
+// store (criar), update, destroy
+
+module.exports = {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const user = await User.create({ email });
+
+    return res.json(user);
+  }
+};
