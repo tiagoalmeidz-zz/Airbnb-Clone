fee1c89841e84d51e9e85ef28be59a49bc09fac7
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-10-08 18:48:55 -0300

    Base da API

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   return res.json('Hello World');
 });
