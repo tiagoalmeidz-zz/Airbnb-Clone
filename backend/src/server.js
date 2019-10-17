c2c41af23d54a7d67b7b42f77d2d96b7c35a6eab
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-10-17 00:24:53 -0300

    Criando rota no servidor para img de spots

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb+srv://tiagoalmeida:gostack@cluster0-opgae.mongodb.net/
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
